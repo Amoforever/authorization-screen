c41678c2ff903ba3c0964e3dbef97af451803d49
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-21 01:17:35 +0300

    Creating gradient.

--- authorization-screen/ViewController.swift
+++ authorization-screen/ViewController.swift
@@ -8,9 +8,22 @@ import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK - gradient
+    private lazy var gradient: CAGradientLayer = {
+            let gradient = CAGradientLayer()
+            gradient.frame = view.bounds
+            gradient.colors = [UIColor.systemBlue.cgColor, UIColor.white.cgColor]
+            gradient.startPoint = CGPoint(x: 0.5, y: 0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 1)
+            return gradient
+        }()
+    private func initialize() {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.layer.insertSublayer(gradient, at: 0)
         // Do any additional setup after loading the view.
     }
 
