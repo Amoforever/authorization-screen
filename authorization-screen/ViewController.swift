1b57c5ad49e6583c3050c97c4fd2a7f13dd15e27
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-22 00:42:01 +0300

    Creating two textField.

--- authorization-screen/ViewController.swift
+++ authorization-screen/ViewController.swift
@@ -33,19 +33,19 @@ extension UITextField {
 
 class ViewController: UIViewController {
     
-// MARK: - gradient
+    // MARK: - gradient
     private lazy var gradient: CAGradientLayer = {
-            let gradient = CAGradientLayer()
-            gradient.frame = view.bounds
-            gradient.colors = [UIColor.systemBlue.cgColor, UIColor.white.cgColor]
-            gradient.startPoint = CGPoint(x: 0.5, y: 0)
-            gradient.endPoint = CGPoint(x: 0.5, y: 1)
-            return gradient
-        }()
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.systemBlue.cgColor, UIColor.white.cgColor]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        return gradient
+    }()
     
     private func initialize() {
         
-// MARK: - label
+        // MARK: - label
         let label = UILabel()
         label.text = "Login"
         label.textColor = .white
@@ -55,17 +55,59 @@ class ViewController: UIViewController {
         label.snp.makeConstraints { maker in
             maker.left.equalToSuperview().inset(150)
             maker.top.equalToSuperview().inset(150)
-            }
+        }
+        
+        //MARK: - firstTextField
         let loginTextField = UITextField()
-        loginTextField.
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.layer.insertSublayer(gradient, at: 0)
-        initialize()
+        loginTextField.textColor = .gray
+        loginTextField.textAlignment = .left
+        loginTextField.placeholder = "keanureevs01"
+        loginTextField.backgroundColor = .white
+        loginTextField.layer.cornerRadius = 25
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(loginTextField)
+        loginTextField.snp.makeConstraints { make in
+            
+            make.top.equalTo(label).inset(70)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+        }
+        
+        let iconImage = UIImage(systemName: "person")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal)
+        if let iconImage = iconImage { loginTextField.setLeftIcon(iconImage)}
+       
+        let markImage = UIImage(systemName: "checkmark.circle.fill")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
+        if let markImage = markImage { loginTextField.setRightIcon(markImage)}
+        
+        //MARK: - secondTextField
+        let passwordTextField = UITextField()
+        passwordTextField.textColor = .gray
+        passwordTextField.textAlignment = .left
+        passwordTextField.placeholder = "password"
+        passwordTextField.backgroundColor = .white
+        passwordTextField.layer.cornerRadius = 25
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(passwordTextField)
+        passwordTextField.snp.makeConstraints { make in
+            
+            make.top.equalTo(loginTextField).inset(70)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+        }
+        let passwordImage = UIImage(systemName: "lock")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal)
+        if let passwordImage = passwordImage{ passwordTextField.setLeftIcon(passwordImage)}
+            
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.layer.insertSublayer(gradient, at: 0)
+            initialize()
+        }
+        
         
     }
-
-
-}
-
